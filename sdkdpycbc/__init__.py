89ef340975c2fec541e059342473daac610d403f
Mark Nunberg <mnunberg@haskalah.org>
2013-10-14 19:42:57 -0700

    Remove __init__.py sys.path manipulation

--- sdkdpycbc/__init__.py
+++ sdkdpycbc/__init__.py
@@ -1,10 +1,1 @@
-import sys
-import os.path
-sys.path.insert(0,
-    os.path.join(
-        os.path.dirname(__file__),
-        "..",
-        "cbsdkd",
-        "lib"))
 
-sys.path.insert(0, "/home/mnunberg/src/python-couchbase-client")
